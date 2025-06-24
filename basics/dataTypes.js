5cd4326f0fbbf3c6c60469e827509f47a0673fa5
pratikadutta <69004865+pratikadutta@users.noreply.github.com>
2025-06-24 09:31:13 +0000

    DataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version
+
+//alert("hello") //we are using nodejs, not browser
+
+let name = "hitesh"
+let age = 18
+let isLoggesIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefiner =>
+//symbol => unique
+
+//object
+
+console.log(typeof "hitesh")
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+console.log(typeof age)
---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "pratika@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
