6726ee9b2be49bd3df706b00b5af06a0e6a1cd4a
Pratika Dutta <69004865+pratikadutta@users.noreply.github.com>
2025-07-01 12:12:29 +0000

    date and times

---
+++ basics/07_dates_and_times.js
@@ -0,0 +1,19 @@
+//Dates
+
+let myDate = new Date()
+
+// console.log(myDate)
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+//let myCreatedDate = new Date(2023,0,23)
+//let myCreatedDate = new Date(2023,5,2,5,3) //6/2/2023, 5:03:00 AM
+
+let myCreatedDate = new Date("01-14-2025")
+console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
