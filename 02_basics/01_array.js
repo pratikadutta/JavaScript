5d0bf4726c3d00e84fcbce8ff9444da762c6a1b3
Pratika Dutta <69004865+pratikadutta@users.noreply.github.com>
2025-07-02 16:14:57 +0000

    Array 1

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,10 +1,38 @@
 //array
 
-const myArray = [0,1,2,3,4,5,6]
+const myArray = [0,1,2,3,4,5]
 const myHerod = ["shaktiman","naagraj"]
 
 const myArray2 = new Array(1,2,3,4)
 
-console.log(myArray[0])
+
 
 //JS array copy operation create shallow copy(reference)
+
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+
+// myArray.unshift(9) //insert at the start of an array
+// myArray.shift() //delete from the start
+
+console.log(myArray.includes(9)); //false
+console.log(myArray.indexOf(9)); //-1
+console.log(myArray.indexOf(5)); //5
+
+const newArry = myArray.join()
+console.log(newArry) //join strings also convert into string
+
+//slice,splice
+
+console.log("A ",myArray)
+const myn1 = myArray.slice(1,3)
+
+console.log(myn1)
+console.log("B ",myArray)
+
+const myn2 = myArray.splice(1,3)
+
+console.log(myn2)
+console.log("C ",myArray)
