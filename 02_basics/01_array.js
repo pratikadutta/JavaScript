5707227dc6df61f0dbeaf36f3994323e2a919ab8
Pratika Dutta <69004865+pratikadutta@users.noreply.github.com>
2025-07-02 04:47:51 +0000

    Array starts

---
+++ 02_basics/01_array.js
@@ -0,0 +1,10 @@
+//array
+
+const myArray = [0,1,2,3,4,5,6]
+const myHerod = ["shaktiman","naagraj"]
+
+const myArray2 = new Array(1,2,3,4)
+
+console.log(myArray[0])
+
+//JS array copy operation create shallow copy(reference)
