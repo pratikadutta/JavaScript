3b0c4893f6765b476c12dc0fea45e26b1fa89356
Pratika Dutta <69004865+pratikadutta@users.noreply.github.com>
2025-07-03 04:53:52 +0000

    Object starts

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,35 @@
+//literal declaration-singleton, constructor-not singleton
+// Object.create - constructor 
+
+//object literals
+
+const mySym = Symbol("key1")
+
+const jsUser = {
+    name:"Pratika",
+    "full name": "Pratika Dutta",
+    [mySym]:"mykey1", //symbol declaration
+    age:23,
+    location:"Kolkata",
+    email:"pratika@gmail.com",
+    isLoggesIn: false,
+    lastLoginDays: ['Monday','Saturday']
+}
+
+console.log((jsUser.email));
+console.log((jsUser["email"]));
+
+console.log(jsUser["full name"])
+console.log(jsUser[mySym]);
+
+jsUser.email = "thepratika@gmail.com"
+Object.freeze(jsUser)
+jsUser.email = "pratika@google.com"
+console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log(("Hello Js User"));
+    
+}
+
+
