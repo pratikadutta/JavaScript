e1626530558ffc2d02893c1bf2882c17452593c5
Pratika Dutta <69004865+pratikadutta@users.noreply.github.com>
2025-07-05 13:50:25 +0000

    Scope 2

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -15,5 +15,42 @@ if (true) {
 // console.log(a);
 // console.log(b);
 console.log(c);
-
 //scope is different in node and browser
+
+function one(){
+  const username = "Pratika"
+  function two() {
+    const website = "youtube"
+    console.log(username);
+  }
+  // console.log(website);
+  
+  two()
+}
+// one()
+
+if (true) {
+  const username = "Pratika"
+  if(username==="Pratika"){
+    const website = " youtube"
+    console.log(username + website);
+    
+  }
+  // console.log(website);
+}
+
+// console.log(username);
+
+//---------------------------interesting -------------------------
+console.log(addOne(5));
+function addOne(num) {
+  return num++
+}
+
+
+
+
+addTwo(5) //error 
+const addTwo = function(num){
+  return num + b2
+}
