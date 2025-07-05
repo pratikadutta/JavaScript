fc7c43262e2e2781d963e7b358ce693a142bf4a3
Pratika Dutta <69004865+pratikadutta@users.noreply.github.com>
2025-07-05 03:58:36 +0000

    Function 2

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -22,7 +22,7 @@ const result = addTwoNumbers(5,6);
 console.log("Result:",result);
 
 function loginUserMessage(username = "Sam"){
-    if(!username) //if(username ===undefined)
+    if(!username) //if(username === undefined)
         {
         console.log("Please enter a username");
         return
@@ -31,3 +31,37 @@ function loginUserMessage(username = "Sam"){
 }
 console.log(loginUserMessage());
 
+
+function calculateCartPrice (...num1)//rest-operator
+{
+    return num1 //[ 200, 400, 500 ]
+}
+console.log(calculateCartPrice(200,400,500));
+
+function calculateCartPrice1 (val1,val2,...num1)//rest-operator
+{
+    return num1 //[ 500, 2000 ]
+}
+console.log(calculateCartPrice1(200,400,500,2000));
+
+const user = {
+    username :"pratika",
+    price: 199
+}
+
+function handleObject (anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.calculateCartPrice}`);
+}
+// handleObject(user)
+handleObject({
+    username:"sam",
+    price:399
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
