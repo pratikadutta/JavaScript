75396cc1e55acbf10eadb81bfa8b3dc029ec8999
Pratika Dutta <69004865+pratikadutta@users.noreply.github.com>
2025-07-08 12:48:37 +0000

    Other Loops

---
+++ 05_iterators/two.js
@@ -0,0 +1,12 @@
+let myArray = ["flash","batman","superman"]
+let arr=0
+while (arr<myArray.length) {
+    console.log(myArray[arr]);
+    arr++
+}
+
+let score=1
+do {
+    console.log(score);
+    score++
+} while (score<=10);
